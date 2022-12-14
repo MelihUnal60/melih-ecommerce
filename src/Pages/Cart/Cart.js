53dbb81470acea2eb150d5afa9df30485f6c7714
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2022-12-14 23:18:10 +0300

    Melih E-commerce

---
+++ src/Pages/Cart/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Cart = () => {
+    return (
+        <h2>Cart Component</h2>
+    )
+    }
+
+
+export default Cart;
---
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProductDetail = () => {
+    return (
+        <h2>productdetail Component</h2>
+    )
+    }
+
+
+export default ProductDetail;
